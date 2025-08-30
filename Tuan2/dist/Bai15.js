dac3c78a223e16d5e6c750b2023aacae5fa70f74
H82M19\Student <Student>
2025-08-30 13:18:51 +0700

    done bai 15

---
+++ Tuan2/dist/Bai15.js
@@ -0,0 +1,22 @@
+"use strict";
+const task1 = async () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Task 1 done");
+        }, 1000);
+    });
+};
+const task2 = async () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Task 2 done");
+        }, 1500);
+    });
+};
+const runSequentialTasks = async () => {
+    const result1 = await task1();
+    console.log(result1);
+    const result2 = await task2();
+    console.log(result2);
+};
+runSequentialTasks();
