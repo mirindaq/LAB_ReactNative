6930adc1b7efe3e405a1de3b935251022b2dd81c
H82M19\Student <Student>
2025-08-30 12:56:49 +0700

    done bai 3

---
+++ Tuan2/src/Bai3.ts
@@ -0,0 +1,12 @@
+function rejectAfterDelay() {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject("Something went wrong");
+    }, 1000); 
+  });
+}
+
+rejectAfterDelay()
+  .catch((error) => {
+    console.log(error); 
+  });
