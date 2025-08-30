c2820fa7f61c761323a0374a5670bb87094ce777
H82M19\Student <Student>
2025-08-30 12:55:51 +0700

    done bai 2

---
+++ Tuan2/dist/Bai2.js
@@ -0,0 +1,11 @@
+"use strict";
+function getNumberAfterDelay() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(10);
+        }, 1000);
+    });
+}
+getNumberAfterDelay().then((result) => {
+    console.log(result);
+});
